5b78ed990a59790d06c5c6d937e92b39d86381fb
ohnoshobu <ohno56sent@gmail.com>
2020-11-13 20:03:49 +0900

    migrationの変更

--- db/migrate/20201030130046_create_hobby_maps.rb
+++ db/migrate/20201030130046_create_hobby_maps.rb
@@ -2,7 +2,7 @@ class CreateHobbyMaps < ActiveRecord::Migration[5.2]
   def change
     create_table :hobby_maps do |t|
       t.references :post, foreign_key: true
-      t.references :hobby, type: :bigint, foreign_key: true
+      t.references :hobby, foreign_key: true
 
       t.timestamps
     end
